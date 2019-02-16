ffe50fdb69b3d248766219e45f1629e9847ed600
dopelord24 <lehatobi@yandex.ru>
2019-02-16 16:01:26 +0300

    Update Population.cs

--- Population.cs
+++ Population.cs
@@ -6,6 +6,12 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
+            if(initialPopulation<=0 || currentPopulation<=0)
+            {
+                throw new ArgumentException();
+                
+            }
+
             int years = 0;
             while (initialPopulation<currentPopulation)
             {
